ecd59c8169e5e59dbbd364b4de1d27aa6ab27f6c
sladeykins-glitch <sladeykins@gmail.com>
2026-09-04 11:35:24 +0100

    Disable nonessential DOM observers to restore responsiveness

--- src/main.tsx
+++ src/main.tsx
@@ -9,25 +9,27 @@ import DatabaseQualityTools from './DatabaseQualityTools'
 import DatabaseQuickActions from './DatabaseQuickActions'
 import DatabaseEditorEnhancements from './DatabaseEditorEnhancements'
 import SiteBackupTools from './SiteBackupTools'
-import DatabaseResultTools from './DatabaseResultTools'
 import DatabaseUndoTools from './DatabaseUndoTools'
 import AccessibilityTools from './AccessibilityTools'
-import DatabasePresetTools from './DatabasePresetTools'
 import DatabaseSelectionTools from './DatabaseSelectionTools'
 import DatabaseCompareTools from './DatabaseCompareTools'
 import DatabaseRecentTools from './DatabaseRecentTools'
-import DatabaseSelectionInsights from './DatabaseSelectionInsights'
 import DataMaintenanceTools from './DataMaintenanceTools'
 import CommandPalette from './CommandPalette'
 import DatabaseCardCopyTools from './DatabaseCardCopyTools'
 import DatabaseCreateTools from './DatabaseCreateTools'
-import AdvancedSearchTools from './AdvancedSearchTools'
-import ItemTaxonomyTools from './ItemTaxonomyTools'
 import DatabaseToolPanel from './DatabaseToolPanel'
 import PwaTools from './PwaTools'
-import OfficialDataAuditTools from './OfficialDataAuditTools'
 import './styles.css'
 
+/*
+ * Keep the always-mounted enhancement layer deliberately small.
+ * Several optional tools previously installed independent document-wide
+ * MutationObservers. With a large bestiary those observers repeatedly scanned
+ * the same cards and made filtering/scrolling feel sluggish. The core workflow
+ * stays mounted here; lower-value analytics/taxonomy/audit/result decorators
+ * can return later as native React features without global DOM observers.
+ */
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
@@ -39,22 +41,16 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <DatabaseQuickActions />
     <DatabaseEditorEnhancements />
     <SiteBackupTools />
-    <DatabaseResultTools />
     <DatabaseUndoTools />
     <AccessibilityTools />
-    <DatabasePresetTools />
     <DatabaseSelectionTools />
     <DatabaseCompareTools />
     <DatabaseRecentTools />
-    <DatabaseSelectionInsights />
     <DataMaintenanceTools />
     <CommandPalette />
     <DatabaseCardCopyTools />
     <DatabaseCreateTools />
-    <AdvancedSearchTools />
-    <ItemTaxonomyTools />
     <DatabaseToolPanel />
     <PwaTools />
-    <OfficialDataAuditTools />
   </React.StrictMode>,
 )
