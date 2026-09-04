2c38b8097f86eca67d34620da8c3a419e4180757
sladeykins-glitch <sladeykins@gmail.com>
2026-09-04 09:38:14 +0100

    Enable database backup and restore controls

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import DatabaseOverlay from './DatabaseOverlay'
+import DatabaseBackupTools from './DatabaseBackupTools'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <DatabaseOverlay />
+    <DatabaseBackupTools />
   </React.StrictMode>,
 )
