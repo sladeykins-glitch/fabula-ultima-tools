e5e95286dd6015e5186fe957f7832ca78c4784b4
sladeykins-glitch <sladeykins@gmail.com>
2026-09-04 10:06:26 +0100

    Enable editor safety and keyboard enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import DatabaseBrowseTools from './DatabaseBrowseTools'
 import DatabasePaginationTools from './DatabasePaginationTools'
 import DatabaseQualityTools from './DatabaseQualityTools'
 import DatabaseQuickActions from './DatabaseQuickActions'
+import DatabaseEditorEnhancements from './DatabaseEditorEnhancements'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -18,5 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <DatabasePaginationTools />
     <DatabaseQualityTools />
     <DatabaseQuickActions />
+    <DatabaseEditorEnhancements />
   </React.StrictMode>,
 )
