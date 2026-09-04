e8d50b85b1791fa41d46dea6222305c97121a3b0
sladeykins-glitch <sladeykins@gmail.com>
2026-09-04 12:33:26 +0100

    Mount native database editor bridge

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import AppErrorBoundary from './AppErrorBoundary'
 import DatabaseOverlay from './DatabaseOverlay'
+import DatabaseOpenEventBridge from './DatabaseOpenEventBridge'
 import SiteBackupTools from './SiteBackupTools'
 import PwaTools from './PwaTools'
 import './styles.css'
@@ -26,6 +27,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </AppErrorBoundary>
     <DatabaseOverlay />
+    <DatabaseOpenEventBridge />
     <SiteBackupTools />
     <PwaTools />
   </React.StrictMode>,
